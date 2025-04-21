30d0d6cde64cbf8569ef4356da05e0e58ae3f89d
PhatBee <157566580+PhatBee@users.noreply.github.com>
2025-04-21 16:23:04 +0700

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -130,7 +130,7 @@ class Inference:
                 rtc_configuration=RTCConfiguration({
                     "iceServers": [
                         {"urls": "stun:stun.l.google.com:19302"},
-                        {"urls": "stun:global.stun.twilio.com:3478?transport=udp"}
+                        {"urls": "stun:global.stun.twilio.com:3478"}
                     ]
                 }),
                 media_stream_constraints={"video": True, "audio": False},
